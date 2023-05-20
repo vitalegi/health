0130cdedb70db6b805f4eb7fe180c9588da9511d
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-05-20 16:41:56 +0200

    mermaid experiment

---
+++ components/Mermaid.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+import mermaid from "mermaid";
+mermaid.initialize({ startOnLoad: false });
+
+const props = defineProps({
+  diagram: String,
+});
+
+const svgHtml = ref("");
+
+const drawDiagram = async function () {
+  const { svg } = await mermaid.render(`mermaid_${Math.round(Math.random()*1000000000)}`, props.diagram);
+  svgHtml.value = svg;
+};
+
+drawDiagram();
+</script>
+
+<template>
+  <div>
+    <div v-html="svgHtml" />
+  </div>
+</template>
